3167a1b0731be238e31be8077221a6a1f4e52889
ksriramreddy <k.sriramreddy9@gmail.com>
2024-09-06 00:42:23 +0530

    nodejs notes

--- 13_loading_html.js
+++ 13_loading_html.js
@@ -1,14 +1,21 @@
 const http = require('http')
 const fs = require('fs')
+const { error } = require('console')
 
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{
         'content-Type':'text/html'
     })
-    const name = "sriram"
-    let html = fs.readFileSync('content/subfolder/index.html','utf-8');
-    res.end(html)
-    // fs.createReadStream('content/subfolder/index.html').pipe(res)// if the html file is is large to load
+    let name = ""
+    // let html = fs.readFileSync('content/subfolder/index.html','utf-8');
+    const stream = fs.createReadStream('content/subfolder/index.html',{encoding:"utf-8"})// if the html file is is large to load
+    stream.on('data',(chunks)=>{
+        name+=chunks
+    })
+    
+    stream.on('end',()=>{
+        res.end(name)
+    })
 })
 
 server.listen(3000,()=>{
